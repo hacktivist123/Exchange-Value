4a941667a26e25610171f773f5a4aed6f07c03fb
hacktivist123 <akintayoshedrack@gmail.com>
2019-10-14 16:15:56 +0100

    apiService fix and display table

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,6 @@ window.addEventListener('load', () => {
     mode: 'history',
     page404: path => {
       const html = errorTemplate({
-        color: 'yellow',
         title: 'Error 404 - Page NOT Found!',
         message: `The path '/${path}' does not exist on this site`
       });
@@ -22,10 +21,38 @@ window.addEventListener('load', () => {
     }
   });
 
-  router.add('/', () => {
-    let html = ratesTemplate();
+// Instantiate api handler
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  timeout: 5000,
+});
+
+// Display Error Banner
+const showError = (error) => {
+  const { title, message } = error.response.data;
+  const html = errorTemplate({ color: 'red', title, message });
+  el.html(html);
+};
+
+// Display Latest Currency Rates
+router.add('/', async () => {
+  // Display loader first
+  let html = ratesTemplate();
+  el.html(html);
+  try {
+    // Load Currency Rates
+    const response = await api.get('/rates');
+    const { base, date, rates } = response.data;
+    // Display Rates Table
+    html = ratesTemplate({ base, date, rates });
     el.html(html);
-  });
+  } catch (error) {
+    showError(error);
+  } finally {
+    // Remove loader status
+    $('.loading').removeClass('loading');
+  }
+});
 
   router.add('/exchange', () => {
     let html = exchangeTemplate();
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();  //read .env files
+require('dotenv').config(); //read .env files
 const express = require('express');
 
-const app = express();  //initialize express app
+const app = express(); //initialize express app
 const port = process.env.PORT || 3000;
 const { getRates } = require('./lib/apiService');
 
@@ -11,18 +11,52 @@ app.use(express.static('public'));
 // Allow front-end access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
+// Express Error handler
+const errorHandler = (err, req, res) => {
+  if (err.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    res
+      .status(403)
+      .send({ title: 'Server responded with an error', message: err.message });
+  } else if (err.request) {
+    // The request was made but no response was received
+    res
+      .status(503)
+      .send({
+        title: 'Unable to communicate with server',
+        message: err.message
+      });
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    res
+      .status(500)
+      .send({ title: 'An unexpected error occurred', message: err.message });
+  }
+};
+
+// Fetch Latest Currency Rates
+app.get('/api/rates', async (req, res) => {
+  try {
+    const data = await getRates();
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+});
+
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
-  console.log('listening on localhost:',port);
+  console.log('listening on localhost:', port);
 });
 
-const test = async() => {
-  const data = await getRates();
-  console.log(data);
-}
+// const test = async() => {
+//   const data = await getRates();
+//   console.log(data);
+// }
 
-test();
-
+// test();
